31040009038a70ec389d2404bc9f885db4da2b8c
shaykhouna <shaykhouna66@gmail.com>
2022-06-30 11:43:10 -0700

    Task 4. Hello, Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+
 /**
  * Puts - This is function print a sentence
  * The sentence is store in the variable helloPuts
  *
- * This function as the first program in C language of this project
+ * helloPuts - This function as the first program in C language of this project
  * This is the task four in project C, Hello, World
  *
- *  Contain one function, the main one
- *  Return: Value zero
+ * Contain one function, the main one
+ * Return: Value zero
  */
 int main(void)
 {
