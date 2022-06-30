c9d939159bdec459bd509daca63e3fdea0b0c858
shaykhouna <shaykhouna66@gmail.com>
2022-06-30 14:29:16 -0700

    Task 5. Hello, Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,14 +3,14 @@
 /**
  * main - nique fonction of this task
  *
- * this program print a sentence done in the task 
- * objective, without using puts only printf
+ * this program print a sentence done in the task objective, 
+ * without using puts only printf
  *
  * Return: will be 0 if success
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a pice of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 }
