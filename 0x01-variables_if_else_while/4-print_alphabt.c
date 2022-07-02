7291674a825a20a83d70f34d670bfbb9279b3d4b
shaykhouna <shaykhouna66@gmail.com>
2022-07-01 18:02:20 -0700

    Task 4fix3

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,9 +11,12 @@ int main(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z' && letter != 'e' && letter != 'q'; ++letter)
+	for (letter = 'a'; letter <= 'z'; ++letter)
 	{
+		if (letter != 'e' && letter != 'q')
+		{
 		putchar(letter);
+		}
 	}
 	putchar('\n');
 	return (0);
