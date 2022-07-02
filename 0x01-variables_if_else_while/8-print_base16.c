f53f35bf6943c6961c253d78b9e2b598ce6a4af4
shaykhouna <shaykhouna66@gmail.com>
2022-07-01 18:53:54 -0700

    Task 8 fix 3

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,7 +13,31 @@ int main(void)
 
 	for (num = 0x0; num <= 0x10; num++)
 	{
-		putchar(num + '0' + 'X');
+		if (num = 10)
+		{
+			num = 'A';
+		}
+		else if (num = 11)
+                {
+                        num = 'B';
+		}
+		else if (num = 12)
+                {
+                        num = 'C';
+                }
+		else if (num = 13)
+                {
+                        num = 'D';
+                }
+		else if (num = 14)
+                {
+                        num = 'E';
+                }
+		else if (num = 15)
+                {
+                        num = 'F';
+                }
+		putchar(num);
 	}
 	putchar('\n');
 	return (0);
