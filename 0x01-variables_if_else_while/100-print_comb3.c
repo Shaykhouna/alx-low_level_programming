c090774800d9f65c227c56adf619267db2a0e0cd
shaykhouna <shaykhouna66@gmail.com>
2022-07-02 05:36:32 -0700

    Task 100 & 102 Advanced

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - combination of digit
+ *
+ * This program prints all possible different combinations of two digits
+ *
+ * Return: return 0 when program successful
+ */
+int main(void)
+{
+	int num1[] = {0..9};
+	int num2[] = {0..9};
+	int comb;
+
+	if (num1 != num2)
+	{
+		comb = {0..9}{0..9};
+	}
+	for (num = 0; num < 99; num++)
+	{
+		if ( ||  )
+		
+		if (num < 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}	
+	}
+	return (0);
+}
