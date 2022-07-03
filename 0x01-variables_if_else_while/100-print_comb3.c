51b46fc38124ae59847e63f184ca77e6ff28833b
shaykhouna <shaykhouna66@gmail.com>
2022-07-03 08:36:16 -0700

    back to retry Task 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,11 +16,13 @@ int main(void)
 	{
 		for (num2 = 0; num2 <= 9; num2++)
 		{
-			if (num1 != num2)
+			if (num1 != num2 && 'num1''num2' != 'num2''num1')
 			{
 			putchar(num1 + '0');
 			putchar(num2 + '0');
 			}
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	return (0);
