119247a341306d20ceecb1e8f0d66fc59a73eb45
shaykhouna <shaykhouna66@gmail.com>
2022-07-05 13:59:23 -0700

    Task0

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - header file
+ *
+ * This is a header file containing the prototypes
+ * and some others
+ *
+ * Return: return 0 as successfully done!
+ */
+ int main(void)
+{
+	return (0);
+}
+
+int _putchar(char obj)
+{
+	putchar(obj);
+	return (0);
+}
+
+
