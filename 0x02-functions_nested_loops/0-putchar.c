8a9c45aec79483402f16051c5285e1e3e2b76699
shaykhouna <shaykhouna66@gmail.com>
2022-07-05 13:46:00 -0700

    Task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Putchar
+ *
+ * This program prints _putchar followed by a new line
+ *
+ * Return: should return 0
+ */
+int main(void)
+{
+	char obj = "_putchar";
+
+	putchar(obj)
+	return (0);
+}
