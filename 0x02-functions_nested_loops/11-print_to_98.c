824bd7b99e9b1c959bbf5c0e67ea649055a5e466
shaykhouna <shaykhouna66@gmail.com>
2022-07-06 15:45:41 -0700

    Task11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - numbers to 98
+ *
+ * This function that prints all natural numbers from n to 98
+ * @n: the variable use as reference
+ *
+ * Return: return 0 when done
+ */
+void print_to_98(int n);
+{
+	int pos;
+
+	if (n >= 98)
+	{
+		for (pos = n; pos >= 98; pos--)
+		{
+			_putchar((pos / 10) + '0');
+			_putchar((pos % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		for (pos = n; pos <= 98; pos++)
+		{
+			_putchar((pos / 10) + '0');
+			_putchar((pos % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+}
