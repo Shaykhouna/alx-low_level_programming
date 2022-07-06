19380f2889547c293efcd3234f03d9112bf427d6
shaykhouna <shaykhouna66@gmail.com>
2022-07-06 14:29:06 -0700

    Task9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,19 +18,39 @@ void times_table(void)
 	{
 		for (col = 0; col <= 9; col++)
 		{
-			res = row * col;
-			if ((res / 10) == 0)
+			if (col == 9)
 			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(res + '0');
+				res = row * col;
+				if ((res / 10) == 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(res + '0');
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(res / 10 + '0');
+					_putchar(res % 10 + '0');
+				}
 			}
 			else
 			{
-				_putchar(' ');
-				_putchar(res + '0');
+				res = row * col;
+				if ((res / 10) == 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(res + '0');
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(res / 10 + '0');
+					_putchar(res % 10 + '0');
+				}
+				_putchar(',');
 			}
-			_putchar(',');
 		}
 		_putchar('\n');
 	}
