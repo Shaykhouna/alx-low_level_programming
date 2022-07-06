a075f763c60bfa4b7bf83a286a7961b3f6181290
shaykhouna <shaykhouna66@gmail.com>
2022-07-06 14:02:25 -0700

    Task8 Federal Agent Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,15 +13,17 @@ void jack_bauer(void)
 	int hour;
 	int minu;
 
-	for (hour = 00; hour <= 23; hour++)
+	for (hour = 0; hour <= 23; hour++)
 	{
-		for (minu = 00; minu <= 59; minu++)
+		for (minu = 0; minu <= 59; minu++)
 		{
-			_putchar(hour + '0');
+			_putchar(hour / 10 + '0');
+			_putchar(hour % 10 + '0');
 			_putchar(':');
-			_putchar(minu + '0');
+			_putchar(minu /10 + '0');
+			_putchar(minu % 10 + '0');
 			_putchar('\n');
 		}
-		minu = 00;
+		minu = 0;
 	}
 }
