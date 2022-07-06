096b49019676a0abf441721ef32bf633420e7826
shaykhouna <shaykhouna66@gmail.com>
2022-07-06 13:34:04 -0700

    Task8 Federal Agent Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - 24h chrono
+ *
+ * This function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+ *
+ * Return: as always 0 when success
+ */
+void jack_bauer(void)
+{
+	int time;
+
+	for (time = 00:00; time <= 23:59; time++)
+	{
+		_putchar(time);
+	}
+}
