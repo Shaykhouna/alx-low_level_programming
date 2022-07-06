aaeacb4f278b3e10d3ecc311206f225794915d6c
shaykhouna <shaykhouna66@gmail.com>
2022-07-05 20:08:21 -0700

    Task3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -12,8 +12,8 @@ int _islower(int c)
 {
 	for (c = 'a'; c <= 'z'; c++)
 		return (1);
-	while (ca = 'A'; ca <= 'Z'; ca++)
-		while (ca = 0; ca <= 9; ca++)
+	while (c = 'A'; c <= 'Z'; c++)
+		while (c = 0; c <= 9; c++)
 			return (0);
 	return (0);
 }
