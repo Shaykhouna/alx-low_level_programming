7830d894a6d3a9baa83c9b7dbf1bcec02ce281fe
shaykhouna <shaykhouna66@gmail.com>
2022-07-06 15:19:24 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - addition
+ *
+ * This function that adds two integers and returns the result
+ * @one: first variable
+ * @two: seconf variable
+ *
+ * Return: the value after addition
+ */
+int add (int one, int two)
+{
+	int res;
+
+	res = one + two;
+	return (res);
+}
