267a3555884b5a68de22fc10e351deb96db2fd06
shaykhouna <shaykhouna66@gmail.com>
2022-07-07 13:26:46 -0700

    Task3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -10,11 +10,12 @@
  */
 void print_numbers(void)
 {
-	char c;
-
-	for (c = 0; c <= 9; c++)
+	for (c = 0; c <= 127; c++)
 	{
-		_putchar(c + '0');
+		if (c >= 0 && c <= 9)
+		{
+			_putchar(c +'0');
+		}
 	}
 	_putchar('\n');
 }
