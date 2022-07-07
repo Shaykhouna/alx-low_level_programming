5d94696e0a01a5cdeb88d2bcfc7213daab219165
shaykhouna <shaykhouna66@gmail.com>
2022-07-07 15:49:30 -0700

    Task4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -13,10 +13,8 @@ void print_most_numbers(void)
 	int c = 0;
 
 	do{
-		while (c != 2 && c != 4)
-		{
+		if (c != 2 || c != 4)
 			_putchar(c + '0');
-		}
 		c++;
 	} while (c < 10);
 	_putchar('\n');
