ac5a0eac0ef9214e7f3be3130bdd0a0a1027cab2
shaykhouna <shaykhouna66@gmail.com>
2022-07-08 05:23:24 -0700

    Task5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,12 +12,11 @@ void more_numbers(void)
 {
 	int c = 0;
 
-	while (c <= 14)
+	for (c = 0; c <= 14; c++)
 	{
 		if (c > 9)
 			_putchar(c / 10 + '0');
 		_putchar(c % 10 + '0');
-		c++;
 	}
 	_putchar('\n');
 }
