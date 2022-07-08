282627f6054376d6601963c67df1004f37718b9f
shaykhouna <shaykhouna66@gmail.com>
2022-07-08 07:58:01 -0700

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,16 +12,17 @@ void more_numbers(void)
 {
 	int long c = 0;
 	int long count = 0;
-	int dec = c / 10;
-	int unit = c % 10;
 
 	for (count = 0; count <= 10; count++)
 	{
 		while (c <= 14)
 		{
 			if (c > 9)
-				_putchar(dec + '0');
-			_putchar(unit + '0');
+			{
+				_putchar(c / 10 + '0');
+				continue;
+			
+			_putchar(c % 10 + '0');
 			c++;
 		}
 		_putchar('\n');
