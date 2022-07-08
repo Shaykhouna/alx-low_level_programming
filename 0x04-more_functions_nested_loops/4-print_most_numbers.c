2f2180830aa3deb20a3f8bd1298772aebfd5eb30
shaykhouna <shaykhouna66@gmail.com>
2022-07-08 04:59:43 -0700

    Task4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,7 +5,7 @@
  * print_most_numbers - numbers
  *
  * This function prints the numbers, from 0 to 9, followed by a new line
- * 
+ *
  * Return: None
  */
 void print_most_numbers(void)
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,18 +8,18 @@
  *
  * Return: just attached by including
  */
-int _isupper(int c);
-
-int _isdigit(int c);
-
-int mul(int a, int b);
+int _putchar(char c);
 
 void print_numbers(void);
 
 void print_most_numbers(void);
 
-int _putchar(char c);
-
 void more_numbers(void);
 
 void print_line(int n);
+
+int _isupper(int c);
+
+int _isdigit(int c);
+
+int mul(int a, int b);
