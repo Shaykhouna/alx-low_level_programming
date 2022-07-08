6aa2c4b7daafadbcd253ba839f0af67306a67094
shaykhouna <shaykhouna66@gmail.com>
2022-07-08 16:02:00 -0700

    Task1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,12 +12,12 @@ int main(void)
 	printf("Infinite loop incoming :\n");
 
 	i = 0;
-	/* this upcoming while loop is infinite due to lack of incrementation */
-	while (1 < 10)
-	{
-		putchar(i);
-	}
-
+	/**
+	 * while (1 < 10)
+	 * {
+	 *	putchar(i);
+	 * }
+	 */
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
