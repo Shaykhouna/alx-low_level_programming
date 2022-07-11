94b448635c6544a93b9652ee7da7d856d1c86ee0
shaykhouna <shaykhouna66@gmail.com>
2022-07-11 12:07:45 -0700

    Task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - printer
+ *
+ * This function prints a string
+ * @str: used as variable
+ *
+ * Return: none return value
+ */
+void _puts(char *str);
+{
+	_putchar('str');
+}
