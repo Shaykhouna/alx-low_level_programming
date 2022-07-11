cf475b6ff4fbcb7d7bf17c136f1c1436da1910de
shaykhouna <shaykhouna66@gmail.com>
2022-07-11 09:31:54 -0700

    Task0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - pointer to int
+ *
+ * This function takes a pointer to an int
+ * as parameter and updates the value it points to to 98
+ * @*n: as variable
+ *
+ * Return: always 0
+ */
+void reset_to_98(int *n)
+{
+	int *n = 98;
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - header file
+ *
+ * This is the Header file
+ *
+ * Return: always nothing
+ */
+int _putchar(char c);
+
+void reset_to_98(int *n);
