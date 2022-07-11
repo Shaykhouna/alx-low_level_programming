c9fffb916a952ebfa0b40712f321a91e6ddb234a
shaykhouna <shaykhouna66@gmail.com>
2022-07-11 16:06:30 -0700

    Task8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_array - printer
+ *
+ * This function prints n elements of an array of integers
+ * @a: variable one
+ * @n: variable two
+ *
+ * Return: none
+ */
+void print_array(int *a, int n);
+{
+	
+}
