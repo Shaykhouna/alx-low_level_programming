f02165ecf6c8326fd9d490f9646dd3dbc0023568
shaykhouna <shaykhouna66@gmail.com>
2022-07-11 17:23:55 -0700

    StandBy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcpy - copier
+ *
+ * This function copies the string pointed to by src
+ * @dest: variable
+ * @src: variable
+ *
+ * Return:
+ */
+char *_strcpy(char *dest, char *src)
+{
+	
+}
