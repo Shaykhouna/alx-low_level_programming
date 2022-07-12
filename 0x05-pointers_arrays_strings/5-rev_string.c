eba0934fc9f58eadc58973b2db19d8d8bdfd23d4
shaykhouna <shaykhouna66@gmail.com>
2022-07-12 06:12:39 -0700

    Task5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,12 +14,14 @@ void rev_string(char *s)
 {
 	int i = 0;
 	char ch = *s;
+	char *str;
 	int len = strlen(s);
 
 	for (i = len - 1; i >= 0; i--)
 	{
 		ch = s[i];
+		str = ch;
 	}
-	*s = ch;
+	s = str;;
 	_putchar('\n');
 }
