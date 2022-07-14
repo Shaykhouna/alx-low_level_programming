72cfc3d54f410008a01393ace79c42492f4311d3
shaykhouna <shaykhouna66@gmail.com>
2022-07-14 14:09:15 -0700

    Task2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -17,7 +17,11 @@ char *_strncpy(char *dest, char *src, int n)
 	while (*src != '\0')
 	{
 		for (i = 0; i < n; i++)
+		{
 			*dest = src[i];
+			dest++;
+			src++;
+		}
 	}
 	*dest = '\0';
 	return(0);
