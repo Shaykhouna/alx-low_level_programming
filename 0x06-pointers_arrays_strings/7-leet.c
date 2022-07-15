c0adeabed756c975f83946eff2ae537b7372a08c
shaykhouna <shaykhouna66@gmail.com>
2022-07-15 02:55:57 -0700

    Task7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -17,7 +17,7 @@ char *leet(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		for (j = 0; from[j] := '\0'; j++)
+		for (j = 0; from[j] != '\0'; j++)
 		{
 			if (str[i] == from[j])
 				str[i] = to[j / 2];
