10ee595a7bf860fbac04faf2be3f62ddd51e69d9
shaykhouna <shaykhouna66@gmail.com>
2022-07-15 13:32:36 -0700

    Task5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -15,9 +15,10 @@ char *string_toupper(char *str)
 
 	while (str[i] != '\0')
 	{
-		if (str[i] > 'a' && str[i] <= 'z')
+		if (str[i] >= 'a' && str[i] <= 'z')
 			str[i] = ('a' - 'A');
 		i++;
 	}
+	_putchar(str);
 	return (str);
 }
