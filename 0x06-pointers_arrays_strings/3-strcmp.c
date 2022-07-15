db99337b8eb0bd2922935cf3a2c719a1c7b12306
shaykhouna <shaykhouna66@gmail.com>
2022-07-15 09:09:26 -0700

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,22 +5,23 @@
  *
  * This function compares two strings
  * @s1: string one
- * s2: string two
+ * @s2: string two
  *
  * Return: return the greater
  */
 int _strcmp(char *s1, char *s2)
 {	
-	while (*s1 == *s2)
+	int i = 0;
+	int diff;
+
+	diff = s1[i] - s2[i];
+	while (s1[i] != '\0' || s2[i] != '\0')
 	{
-		if (*s1 == '\0' && *s2 != '\0')
-			return (-15);
-		else if (*s1 != '\0' && *s2 == '\0')
-			return (15);
-		s1++;
-		s2++;
+		if (s1[i] != s2[i])
+			return (diff);
+			break;
+		i++;
 	}
-	if (*s1 == '\0' && s2 == '\0')
-		return (0);
+	return (0);
 }
 
