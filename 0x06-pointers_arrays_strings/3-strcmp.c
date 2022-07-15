5f0b62c897362e8f45b17eb9bd3bec4b70fe4f3b
shaykhouna <shaykhouna66@gmail.com>
2022-07-15 10:21:58 -0700

    Task

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,9 +11,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {	
-	if (s1 != s2)
-		return (s1[i] - s2[i]);
-	else
-		return (0);
+	int i = 0;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (0);
 }
 
