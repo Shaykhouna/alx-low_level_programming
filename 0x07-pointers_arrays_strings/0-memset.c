a76185d48fb7dd01c42937ecee1086c5dbd0d022
shaykhouna <shaykhouna66@gmail.com>
2022-07-18 15:37:03 -0700

    Task0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -13,14 +13,11 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	int i;
-	char *tmp;
 
-	*tmp = s;
 	while (n != 0)
 	{
 		for (i = 0; i < n; i++)
-			s[i] = b;
-		*s = *tmp;
+			s[i] = 'b';
 	}
 	return (s);
 }
