bbc9e9f6bf3008f1414f5d1359d7cd6b3498c889
shaykhouna <shaykhouna66@gmail.com>
2022-07-19 09:48:39 -0700

    Task6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -14,17 +14,15 @@ void print_chessboard(char (*a)[8])
 	int i;
 	int j;
 
-	for (i = 0; a[i][8]; i++)
+	while (i < 8)
 	{
-		for (j = 0; j < 8; j++)
+		j = 0;
+		while (j < 8)
 		{
-			if (j == 7 )
-			{
-				_putchar(a[i][j]);
-				_putchar('\n');
-			}
-			else
-				_putchar(a[i][j];
+			_putchar(a[i][j]);
+			j++;
 		}
+		_putchar('\n');
+		i++;
 	}
 }
