cb4b279a91b06af8b9513faa002c164e9e3662a4
shaykhouna <shaykhouna66@gmail.com>
2022-07-19 06:25:43 -0700

    Task1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -14,9 +14,10 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 {
 	unsigned int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
-		dest[i] = src[i];
+		if (s[i] == c)
+			return (s + i);
 	}
-	return (dest);
+	return ('\0');
 }
