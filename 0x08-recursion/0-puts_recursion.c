76359b1b7c52f210c36687cbcf7bde73cf8dbff4
shaykhouna <shaykhouna66@gmail.com>
2022-07-20 03:19:04 -0700

    Task0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -12,8 +12,12 @@ void _puts_recursion(char *s)
 {
 	int i = 0;
 
-	_putchar(s[i]);
-	s++;
-	_puts_recursion(s);
-	_putchar('\n');
+	if (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		s++;
+		_puts_recursion(s);
+	}
+	else
+		_putchar('\n');
 }
