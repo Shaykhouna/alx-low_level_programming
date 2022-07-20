6b71e95ea31a77a4bd9ce20580048c97c0765976
shaykhouna <shaykhouna66@gmail.com>
2022-07-20 07:15:15 -0700

    Task4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -15,6 +15,8 @@ int _pow_recursion(int x, int y)
 
 	if (y == 0)
 		return (1);
+	else if (y < 0)
+		pow = x * _pow_recursion(x, (y + 1));
 	pow = x * _pow_recursion(x, (y - 1));
 	return (pow);
 }
