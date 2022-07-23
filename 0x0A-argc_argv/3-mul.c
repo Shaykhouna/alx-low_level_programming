564346f276111547dd89d3d75ce033c0a3599704
shaykhouna <shaykhouna66@gmail.com>
2022-07-23 14:21:22 -0700

    Task3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -19,6 +19,11 @@ int main(int argc, char *argv[])
 
 	if (argc > 1)
 	{
+		if (argc == 2)
+		{
+			printf("%d\n", argv[1]);
+			break;
+		}
 		a = atoi(argv[1]);
 		b = atoi(argv[2]);
 		printf("%d\n", a * b);
