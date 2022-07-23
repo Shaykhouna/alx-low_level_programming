6e84e1fa908901db6c54b6fdd24538470f7d5839
shaykhouna <shaykhouna66@gmail.com>
2022-07-22 18:50:54 -0700

    Task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - name
+ *
+ * This program prints its name, followed by a new line
+ * @argc: count
+ * @argv: array
+ *
+ * Return: value int
+ */
+int main(int argc, char *argv[])
+{
+	_putchar(argv[0];
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Header
+ *
+ * This is header file
+ *
+ * Return: nothing.
+ */
+int main(int argc, char *argv[]);
+
+int _putchar(char c);
+
+
