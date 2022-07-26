8c361e89159ec0e72a18c991c55847df3b89f827
shaykhouna <shaykhouna66@gmail.com>
2022-07-26 15:40:26 -0700

    TAsk1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -20,8 +20,7 @@ char *_strdup(char *str)
 	new = malloc(sizeof(*str) * 2);
 	for (i = 0; i < strlen(str); i++)
 	{
-		new = str[i];
-		new++;
+		new[i] = str[i];
 	}
 	new = &str;
 	if (sizeof(new) < sizeof(str))
