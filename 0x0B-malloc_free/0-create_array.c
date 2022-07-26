959eb264e246a0566f41739e21140e716a098389
shaykhouna <shaykhouna66@gmail.com>
2022-07-25 18:57:43 -0700

    Task0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,8 +15,8 @@ char *create_array(unsigned int size, char c)
 	char *array;
 
 	array = malloc(sizeof(char) * size);
-	if (size == 0);
+	if (size == 0)
 		return (NULL);
-	array[c];
+	array[0] = c;
 	return (array);
 }
