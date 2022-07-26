1c5c6c90a40fb8824c0ec7a98d9c0e9753e89b75
shaykhouna <shaykhouna66@gmail.com>
2022-07-25 20:02:51 -0700

    Task1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * _strdup - pointer giver
@@ -12,6 +13,7 @@
 char *_strdup(char *str)
 {
 	char **new;
+	int i;
 
 	if (str == NULL)
 		return (NULL);
