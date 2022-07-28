7f012ac7221b90b684604eb49c959199aac894c2
shaykhouna <shaykhouna66@gmail.com>
2022-07-28 13:44:05 -0700

    Task0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * malloc_checked - allocator
+ *
+ * This function allocates memory using malloc
+ * @b: size to use
+ *
+ * Return: nothing
+ */
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *check;
+
+	void *check = malloc(b);
+	if (ckeck == NULL && b > 0) Handle_failure(98);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,22 @@
+This is THE README File 
+Here you could check the role and function of the differents programs There
+
+
+0. Trust no one
+0-malloc_checked.c
+
+
+
+1. string_nconcat
+1-string_nconcat.c
+
+
+2. _calloc
+2-calloc.c
+
+
+3. array_range
+3-array_range.c
+
+
+
