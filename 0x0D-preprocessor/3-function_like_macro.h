be00342e73bb5703d81d3f49a10ff0a5b9cdf2b4
shaykhouna <shaykhouna66@gmail.com>
2022-08-01 16:01:36 -0700

    Task3

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef ABS(x)
+#define ABS(x) (((x) < 0) ? -(x) : (x))
+
+/**
+ * main - absolute
+ *
+ * This function-like macro ABS that computes the absolute value of a number
+ *
+ * Return: nada
+ */
+
+#endif /* ABS */
