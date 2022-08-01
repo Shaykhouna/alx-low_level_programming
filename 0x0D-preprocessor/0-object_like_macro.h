1a1adc0e79c592496e6fe748ec5aab4fe22ccd07
shaykhouna <shaykhouna66@gmail.com>
2022-08-01 14:57:29 -0700

    Task0

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -2,9 +2,11 @@
 #define SIZE 1024
 
 /**
- * HEADER FILE
+ * main - header file
  *
  * This header defines a macro named SIZE as an abbreviation for the token 1024
+ *
+ * Return: nada.
  */
 
 #endif /* SIZE */
