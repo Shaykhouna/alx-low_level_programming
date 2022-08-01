ee27da4b6610c0c59c00c27bd784ae528f720fb7
shaykhouna <shaykhouna66@gmail.com>
2022-08-01 15:14:36 -0700

    Task2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define __BASE_FILE__
+
+/**
+ * main - name printer
+ *
+ * This program prints the name of the file it was compiled from
+ *
+ * Return: 0 when done
+ */
+void main(void)
+{
+	_putchar("__BASE_FILE__");
+	return (0);
+}
