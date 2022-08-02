19f995e658fe56368b101ce9ce3e6fbfe4f29a65
shaykhouna <shaykhouna66@gmail.com>
2022-08-01 19:48:27 -0700

    Task0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,11 @@
+/**
+ * struct dog - poppy
+ * @name: first ember
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: a new type struct dog with theabove
+ */
 struct dog {
 	char *name;
 	float age;
