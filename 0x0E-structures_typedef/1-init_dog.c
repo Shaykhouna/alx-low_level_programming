addb6a62332142177fccb752479f01e3fb80ca74
shaykhouna <shaykhouna66@gmail.com>
2022-08-02 06:19:41 -0700

    Task1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * init_dog - initializer
+ *
+ * This function that initialize a variable of type struct dog
+ * @d: structure
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Return: nada
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	
+}
---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - Header File
+ *
+ * This is the header file of prototypes
+ *
+ * Return: nada
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif /* MAIN_H */
