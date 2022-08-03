9a8ab1d0c688af14eae6c0647cfd07e6da7a26e5
shaykhouna <shaykhouna66@gmail.com>
2022-08-03 16:08:16 -0700

    Task1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,9 +14,12 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	int i, j;
 
 	for (i = 0; i < size; i++)
-		action(array[i]);
+	{
+		j = array[i];
+		action(j);
+	}
 	return;
 }
