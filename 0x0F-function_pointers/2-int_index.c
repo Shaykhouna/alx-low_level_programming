4e1231382aadbc3f59e2067332af9a6f6fe1bfc5
shaykhouna <shaykhouna66@gmail.com>
2022-08-03 16:42:20 -0700

    Tsk2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,1 +1,26 @@
-:x
+#include "function_pointers.h"
+
+/**
+ * int_index - search
+ *
+ * This function that searches for an integer
+ * @array: location
+ * @size: array size
+ * @cmp: comparator
+ *
+ * Return: the integer found
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+	}
+	return (-1);
+}
