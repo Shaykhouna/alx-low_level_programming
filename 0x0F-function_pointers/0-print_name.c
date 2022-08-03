4e343dffc5a75c1334f6c89bfa939e3930e879ea
shaykhouna <shaykhouna66@gmail.com>
2022-08-03 15:40:53 -0700

    Task0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "function_pointers.h"
 
 /**
@@ -13,6 +14,12 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	char str;
+	int i, len;
+
+	str = f(name);
+	len = strlen(name);
+	for (i = 0; i < len; i++)
+		_putchar(str[i]);
 	return;
 }
