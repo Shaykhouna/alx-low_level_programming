57d9c505226af00bb5c2fcbb6575fcc651365b72
shaykhouna <shaykhouna66@gmail.com>
2022-08-26 16:03:19 -0700

    Task0

--- 0x15-file.io/0-read_textfile.c
+++ 0x15-file.io/0-read_textfile.c
@@ -14,11 +14,24 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file;
+	ssize_t ofile, rfile, wfile;
+	char *buf;
 
 	if (filename == NULL)
+		return (0);
+	buf = malloc(sizeof(chr) * letters);
+	if (buffer == NULL)
+		return (0);
+	ofile = open(filename, O_RDONLY);
+	rfile = read(ofile, buf, letters);
+	wfile = write(STDOUT_FILENO, buf, r);
+
+	if (ofile == -1 || rfile == -1 || wfile == -1 || wfile != rfile)
+	{
+		free(buf);
 		return 0;
-	file = open(filename, O_RDWR);
-	if (file == -1)
-		return 0;
+	}
+	free(buf);
+	return (0);
+	return (wfile);
 }
