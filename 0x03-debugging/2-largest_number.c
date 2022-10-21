89395975d96fe746cc42acde981d99f2e7480556
Cheikh DIALLO <shaykhouna66@gmail.com>
2022-10-21 15:12:17 -0700

    0 > 972?

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,22 +12,21 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a != b && b != c)
+	if (a != b && b != c && a != c)
 	{
-		if (a >= b && a >= c)
+		if (a > b && a > c)
 		{
 			largest = a;
 		}
-		else if (b >= a && b >= c)
+		else if (b > a && b > c)
 		{
 	        	largest = b;
 		}
-		else if (c >= a && c >= b)
+		else if (c > a && c > b)
 		{
 			largest = c;
 		}
 	}
-	else
-		_putchar('\n');
+	_putchar('\n');
 	return (largest);
 }
