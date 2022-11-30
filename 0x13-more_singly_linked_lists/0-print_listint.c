fdf3eb5d0aaf210d0172055996d061a3328ec55f
Cheikh DIALLO <shaykhouna66@gmail.com>
2022-11-29 16:04:38 -0800

    0. Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,12 +10,12 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t node = 0;
+	size_t nodes = 0;
 
 	while (h)
 	{
-		node++;
-		printf("%d\n", h->n);
+		nodes++;
+		printf("%d\n", h);
 		h = h->next;
 	}
 	return (node);
