30a41d9ff5c5a52269a4b8591385a6e3416abcdb
Cheikh DIALLO <shaykhouna66@gmail.com>
2022-12-05 02:00:34 -0800

    Late modifications

--- 0x15-file.io/0-read_textfile.c
+++ 0x15-file.io/0-read_textfile.c
@@ -1,10 +1,5 @@
 #include "main.h"
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-
 /**
  * read_textfile - reader
  * @filename: file to act upon
@@ -14,14 +9,10 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t rfile, wfile;
-	int file;
+	ssize_t ofile, rfile, wfile;
 	char *buf;
 
 	if (filename == NULL)
-		return (0);
-	file = open(filename, O_RDONLY);
-	if (file == -1)
 		return (0);
 	buf = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
@@ -29,13 +20,22 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		free(buf);
 		return (0);
 	}
+	ofile = open(filename, O_RDONLY)
+	if (ofile == -1)
+	{
+		free(buf);
+		return (0);
+	}
 	rfile = read(file, buffer, letters);
 	if (rfile == -1)
+	{
+		free(buf);
 		return (0);
+	}
 	wfile = write(STDOUT_FILENO, buf, rfile);
 	if (wfile == -1 || rfile != wfile)
 		return (0);
 	free(buf);
-	close(file);
+	close(ofile);
 	return(wfile);
 }
