0e47d6a605ce6e42e1d101737c8e90d391180b23
Cheikh DIALLO <shaykhouna66@gmail.com>
2023-02-18 14:07:41 -0800

    Fixed lapsus

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,17 +17,18 @@ int main(void)
 	{
 		for (num2 = 0; num2 <= 9; num2++)
 		{
-			if ((num1 != num2 && num1 < num2) || (num1 == 8 && num2 == 9))
+			if (num1 != num2 && num1 < num2)
 			{
 				putchar(num1 + '0');
 				putchar(num2 + '0');
-				if (num1 != 8 && num2 != 9)
+				if (num1 != 8 && num2 != 9 && num1 < num2)
 				{
 					putchar(',');
 					putchar(' ');
 				}
 			}
 		}
+		putchar(',');
 	}
 	return (0);
 }
