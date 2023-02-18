281fbc2e8020be0c9bab089da2a17b1027f51787
Cheikh DIALLO <shaykhouna66@gmail.com>
2023-02-18 14:36:10 -0800

    Fixed lapsus

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -21,11 +21,11 @@ int main(void)
 			{
 				putchar(num1 + '0');
 				putchar(num2 + '0');
-			}
-			if (num1 != 8 && num2 != 9)
-			{
-				putchar(',');
-				putchar(' ');
+				if (num1 != 8 && num2 != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
