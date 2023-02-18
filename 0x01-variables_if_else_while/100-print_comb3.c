abe15514105e6d9c500f212c5e37a51b40c4ca2f
Cheikh DIALLO <shaykhouna66@gmail.com>
2023-02-18 07:16:18 -0800

    fixed code

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,20 +12,20 @@ int main(void)
 {
 	int num1;
 	int num2;
-	int list[100];
 
 	for (num1 = 0; num1 < 9; num1++)
 	{
 		for (num2 = 0; num2 <= 9; num2++)
 		{
-			if (num1 != num2)
+			if (num1 != num2 && num < num2)
 			{
-			putchar(num1 + '0');
-			putchar(num2 + '0');
-			putchar(',');
-			putchar(' ');
-			append(list, "'num1''num2'");
-			putchar(list[100]);
+				putchar(num1 + '0');
+				putchar(num2 + '0');
+				if (num1 != 8 && num2 != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
