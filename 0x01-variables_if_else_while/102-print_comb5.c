9c7874083f15f5de37acec2e2cf672cf557b5986
Cheikh DIALLO <shaykhouna66@gmail.com>
2023-02-18 16:36:04 -0800

    Recode task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -24,7 +24,7 @@ int main(void)
 			{
 				for (p = 0; p <= 9; p++)
 				{
-					if (n <= o && m < p)
+					if ((n <= o && m < p) || (n < o && m <= p))
 					{
 						putchar(n + '0');
 						putchar(m + '0');
