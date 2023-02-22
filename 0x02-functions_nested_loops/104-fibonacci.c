120d4665c77c6b8f9c2d6f13fa56bd1d4fae35be
Cheikh DIALLO <shaykhouna66@gmail.com>
2023-02-22 14:35:31 -0800

    Last Task adv Fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ *
+ * This program finds and prints the first 98 Fibonacci numbers
+ *
+ * Return: 0 when success
+ */
+int main(void)
+{
+	long int prev = 0;
+	long int new = 1;
+	long int sum = 0;
+	long int count = 0;
+
+	while (count <= 98)
+	{
+		sum = new + prev;
+		if (count < 98)
+		{
+			printf("%ld, ", sum);
+		}
+		else
+		{
+			printf("%ld\n", sum);
+		}
+		prev = new;
+		new = sum;
+	}
+	return (0);
+}
