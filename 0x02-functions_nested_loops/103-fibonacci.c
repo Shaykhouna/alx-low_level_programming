b876bd95f3f3c1d8fa7f0d5701f964973522ff26
Cheikh DIALLO <shaykhouna66@gmail.com>
2023-02-22 13:54:28 -0800

    Try for 4000000 Fibonacci Top

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Fibonacci top 50
+ *
+ * This program prints the first 50 Fibonacci numbers, starting with 1 and 2
+ *
+ * Return: Always 0 when success
+ */
+int main(void)
+{
+	int count = 0;
+	long int prev = 0;
+	long int new = 1;
+	long int sum = 0;
+
+	while (sum <= 4000000)
+	{
+		sum = new + prev;
+		if (4000000 - new < prev)
+                {
+                        printf("%ld, ", sum);
+                }
+                else
+                {
+                        printf("%ld\n", sum);
+                }
+                prev = new;
+                new = sum;
+        }
+        return (0);
+}
