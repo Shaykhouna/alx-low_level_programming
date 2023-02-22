6dcbe7df334fe705c3b7aacd6ddeae3340f737a2
Cheikh DIALLO <shaykhouna66@gmail.com>
2023-02-22 06:01:22 -0800

    Try advanced one

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_times_table - print times table 
+ *
+ * This function prints times table of the given number
+ * @n: the given number to print times table for
+ *
+ * Returns: nothing due to void
+ */
+void print_times_table(int n)
+{
+	if (n <= 15 && n >= 0)
+	{
+		for (one = 0, one <= n, one++)
+		{
+			int count = 0;
+			for (two = 0, two <= n, two++)
+			{
+				tim = one * two;
+				if (two < n)
+				{
+					if (tim >= 10)
+					{
+						printf("%d,  ", tim);
+					}
+					else if (tim >= 100)
+					{
+						printf("%d, ", tim);
+					}
+				}
+				else
+				{
+					printf("%d\n", tim);
+				}
+			}
+		}
+	}
+}
