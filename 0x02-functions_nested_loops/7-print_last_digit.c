a6762aae7abad6d252cb62af917d0f9f148e85a3
Cheikh DIALLO <shaykhouna66@gmail.com>
2023-02-22 05:15:38 -0800

    Recoded

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,9 +13,13 @@ int print_last_digit(int c)
 {
 	int digit;
 
-	if (c < 0 or c == INT_MIN)
+	if (c < 0)
 	{
 		c = c * (-1);
+	}
+	if (c == INT_MIN)
+	{
+		c = -(c % 10);
 	}
 	digit = c % 10;
 	_putchar(digit + '0');
