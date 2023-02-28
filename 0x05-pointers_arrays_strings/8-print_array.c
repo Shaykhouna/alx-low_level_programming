779ba21d5d04a2ee3b8e1f3c3cfcd34541e8ec06
Cheikh DIALLO <shaykhouna66@gmail.com>
2023-02-28 14:45:36 -0800

    8 print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,13 +13,19 @@ void print_array(int *a, int n)
 {
 	int i;
 	char cnt = *a;
+	int len = strlen(*a);
 
 	for (i = 0; i < n; i++)
 	{
 		cnt = a[i];
-		_putchar(cnt + '0');
-		_putchar(',');
-		_putchar(' ');
+		if (i != len - 1)
+		{
+			printf("%d, ", cnt);
+		}
+		else
+		{
+			printf("%d", cnt);
+		}
 	}
 	_putchar('\n');
 }
