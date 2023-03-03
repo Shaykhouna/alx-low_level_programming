2ad3d2fcd7dfb1b745c71f27d321a5b89887c4e4
Cheikh DIALLO <shaykhouna66@gmail.com>
2023-03-03 15:32:56 -0800

    refix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -17,7 +17,7 @@ char *_strncat(char *dest, char *src, int n)
 
 	while (*dest)
 		dest++;
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; i < n; i++)
 	{
 		dest[len - 1] = src[i];
 		len++;
