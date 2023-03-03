94d57b499dd30f123b17a2b026f774436308f376
Cheikh DIALLO <shaykhouna66@gmail.com>
2023-03-03 15:38:13 -0800

    refix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * _strncat - concatenater
@@ -13,7 +12,7 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int i;
-	int len = strlen(dest);
+	int len = _strlen(dest);
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
