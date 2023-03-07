3846e1eb99059a2832ba3116d5c184d54a457776
Cheikh DIALLO <shaykhouna66@gmail.com>
2023-03-07 15:02:40 -0800

    take it back

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,13 +11,13 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i = 0;
-	int j = 0;
+	unsigned int i = 0;
+	unsigned int j = 0;
 	int leng;
 
 	while (s[i] != '\0')
 	{
-		for (j = 0; accept[i] != '\0'; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
