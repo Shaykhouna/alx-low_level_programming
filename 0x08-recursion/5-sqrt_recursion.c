73818b3bc8d2031b1e83da6373101213429c4511
shaykh <shaykhouna66@gmail.com>
2023-03-08 23:45:27 +0100

    try again

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -20,16 +20,13 @@ int _sqrt_recursion(int n)
 		return (0);
 	else if (n == 1)
 		return (1);
-	else
-	{
-		if ((rt1 % 2 == 0) && (rt1 * rt1 == n))
-			return (rt1);
-		else if (rt1 % 2 == 0)
-			return (_sqrt_recursion(rt1));
-		else if ((rt2 % 3 == 0 || rt2 * rt2 == n))
-			return (rt2);
-		else if (rt2 % 3 == 0)
-			return (_sqrt_recursion(rt2));
-		return (-1);
-	}
+	if ((rt1 % 2 == 0) && (rt1 * rt1 == n))
+		return (rt1);
+	else if (rt1 % 2 == 0)
+		return (_sqrt_recursion(rt1));
+	else if ((rt2 % 3 == 0 || rt2 * rt2 == n))
+		return (rt2);
+	else if (rt2 % 3 == 0)
+		return (_sqrt_recursion(rt2));
+	return (-1);
 }
