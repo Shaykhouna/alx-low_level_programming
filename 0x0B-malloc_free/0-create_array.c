5260928f09ea50b3098016a2f5e9e2c91158ec80
Cheikh DIALLO <shaykhouna66@gmail.com>
2023-03-15 15:55:08 -0700

    ixed Code

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,7 +19,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	array = malloc(sizeof(char) * size);
 	if (array == NULL) {
-		return (1)
+		return (NULL)
 	}
 	for (i = 0; i < size; i++)
 		array[i] = c;
